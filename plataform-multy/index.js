cf5dd92e532fc03d34f292e780139b8117eef4d0
yanelysvelarde <yanelys932@gmail.com>
2023-10-07 04:15:33 -0400

    animation

--- plataform-multy/index.js
+++ plataform-multy/index.js
@@ -26,7 +26,21 @@ constructor(){      //the constructor method sets the properties
 draw(){             //in here I'm making like a square that will represent later the player
 c.fillRect(this.position.x,this.position.y, this.width, this.height)
 }
+
+update(){   //altering the player/s properties
+this.position.y += this.velocity.y
+this.draw()
+
+
+}
+
 }
 
 const player = new Player()       //in here i want to implement the Player class
-player.draw()
+
+
+function animate(){
+requestAnimationFrame(animate)   //requestAnimationFrame acts like loop...kinda, so it's calling animate an infinity number of times 
+player.update()
+}
+animate()
