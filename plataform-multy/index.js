b2987af81c61e56a83fe4b0d19a21ed1070eb727
yanelysvelarde <yanelys932@gmail.com>
2023-10-07 03:30:45 -0400

    created a class of player

--- plataform-multy/index.js
+++ plataform-multy/index.js
@@ -1,3 +1,13 @@
 const canvas= document.querySelector('canvas')
 const c = canvas.getContext('2d')
 console.log(c)
+
+class Player{      //this is a class of Player. in here we add the properties that defines the player
+constructor(){      //the constructor method sets the properties
+
+
+}
+
+
+
+}
