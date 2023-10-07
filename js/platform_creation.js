24c24b4206262c59b2b4242d9eaf299031f313ae
eduard-a1 <142949203+eduard-a1@users.noreply.github.com>
2023-10-07 19:51:16 -0400

    FIXED Changes

--- js/platform_creation.js
+++ js/platform_creation.js
@@ -1,17 +1,18 @@
-class Platform{
-    constructor(position={x:100,y:100},color ='red'){      //the constructor method sets the properties
-        this.position= position;
-        this.lastKey = null;
-    
-        this.width = 300
-        this.height = 10
+class Platform {
+  constructor(position = { x: 100, y: 100 }, color = "red") {
+    //the constructor method sets the properties
+    this.position = position;
+    this.lastKey = null;
 
-        this.color = color
-    }
-    draw(){             //in here I'm making like a square that will represent later the player
-        c.fillStyle=this.color
-            c.fillRect(this.position.x,this.position.y, this.width, this.height)
-        }
-    }
+    this.width = 300;
+    this.height = 10;
 
-    
+    this.color = color;
+  }
+  draw() {
+    //in here I'm making like a square that will represent later the player
+    c.fillStyle = this.color;
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+}
