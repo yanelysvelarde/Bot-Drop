57f649d9701990000f8b0baafce306ea408fc9f9
majo-marroquin <marr.mariajose@gmail.com>
2023-10-07 20:59:32 -0400

    changes under enemies. started with just the foundation.

---
+++ js/enemies.js
@@ -0,0 +1,20 @@
+// File is for the different types of enemies
+/*for one of the enemies should i add a speed to it 
+so that it goes back and forth?*/
+class enemy_type_1{
+    constructor (x, y, w, h, color = 'white'){ /* x and y will be this enemies location.
+     w and h is width and height. color is specific to this enemy which is white.
+     these are set as their own variables so that they can be changed later. */
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+// function renderEnemy(){
+// pass
+// }
+
+// function animate(){
+// pass
+// }
+}
